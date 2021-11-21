526b439f084297a8cd8bae494fbb2e5163d60359
Jason Ash <ash.jasont@gmail.com>
2021-11-21 10:27:12 -0800

    make Nav component a ul

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,6 +1,9 @@
 <div>
   <nav>
-    <a href="/"><h1>Jason Ash</h1></a>
-    <a href="/about"><h1>About</h1></a>
+    <ul>
+      <li><a href="/">Jason Ash</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/resume">Resume</a></li>
+    </ul>
   </nav>
 </div>
