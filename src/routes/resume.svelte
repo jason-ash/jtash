7bf7b0b354a3d7af1d1e30c323c6e1d0233ddc62
Jason Ash <ash.jasont@gmail.com>
2021-11-27 15:08:06 -0800

    start fleshing out the resume page

---
+++ src/routes/resume.svelte
@@ -0,0 +1,83 @@
+<section class="profile">
+  <h2>profile</h2>
+  <p>
+    Data scientist and actuary with advanced financial modeling, forecasting, and
+    analytical expertise. Experience managing teams to monitor complex risk, operate
+    financial hedging programs, improve operational and marketing processes, and develop
+    and launch new business initiatives. Seeking to contribute to a team by identifying
+    valuable strategic improvements using analytical and predictive models.
+  </p>
+</section>
+
+<section class="open-source">
+  <h2>open source</h2>
+  <ul>
+    <li><a href="https://frm.milliman.com/en/">pyesg</a></li>
+    <li><a href="https://github.com/jason-ash/playful">playful</a></li>
+  </ul>
+</section>
+
+<section class="work-experience">
+  <h2>work experience</h2>
+  <ul>
+    <li>
+      VP Data & Analytics
+      <a href="https://www.symetra.com">Symetra</a>
+    </li>
+    <li>
+      Founder at
+      <a href="https://www.ashanalytics.com">Ash Analytics</a>
+    </li>
+    <li>
+      Head of Insurance and Analytics at
+      <a href="https://www.leaplife.com">Leaplife</a>
+    </li>
+    <li>
+      Senior Investor Analytics Manager at
+      <a href="https://www.lendingclub.com">LendingClub</a>
+    </li>
+    <li>
+      Consulting Actuary at
+      <a href="https://frm.milliman.com/en/">Milliman</a>
+    </li>
+  </ul>
+</section>
+
+<section class="publications">
+  <h2>publications</h2>
+  <ul>
+    <li>
+      <a href="https://theactuarymagazine.org/creative-landscape/">Creative Landscape</a>
+      — featured article in The Actuary magazine
+    </li>
+    <li>
+      <a href="/">Bayesian Models in Insurance</a>
+      — presenter at SOA Predictive Analytics Symposium
+    </li>
+    <li>
+      <a href="/">Improving Actuarial Communication</a>
+      — presenter at SOA Valuation Actuary Symposium
+    </li>
+  </ul>
+</section>
+
+<section class="skills">
+  <h2>skills</h2>
+  <ul>
+    <li>Python</li>
+  </ul>
+</section>
+
+<section class="professional-designations">
+  <h2>professional designations</h2>
+  <ul>
+    <li>Fellow of the Society of Actuaries (FSA)</li>
+    <li>Chartered Enterprise Risk Analyst (CERA)</li>
+    <li>Member of the American Academy of Actuaries (MAAA)</li>
+  </ul>
+</section>
+
+<section class="education">
+  <h2>education</h2>
+  <p>Northwestern University</p>
+</section>
