26920287a844c7bd8365595e7c6023fc66c4882b
Jason Ash <ash.jasont@gmail.com>
2021-11-27 15:07:34 -0800

    extra formatting for code snippets in post layout

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -20,24 +20,4 @@
   console.log(post);
 </script>
 
-<svelte:head>
-  <link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.min.css"
-    integrity="sha384-R4558gYOUz8mP9YWpZJjofhk+zx0AS11p36HnD2ZKj/6JR5z27gSSULCNHIRReVs"
-    crossorigin="anonymous"
-  />
-  <script
-    defer
-    src="https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.min.js"
-    integrity="sha384-z1fJDqw8ZApjGO3/unPWUPsIymfsJmyrDVWC8Tv/a1HeOtGmkwNd/7xUS0Xcnvsx"
-    crossorigin="anonymous">
-  </script>
-  <style>
-    .katex {
-      font-size: 1.05em !important;
-    }
-  </style>
-</svelte:head>
-
 <PostLayout {post} />
