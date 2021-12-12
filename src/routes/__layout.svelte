72d24413ed8cb39e2664531a1bedb73bebb370cc
Jason Ash <ash.jasont@gmail.com>
2021-12-11 17:14:23 -0800

    add container styling to __layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,29 @@
   import Nav from "../lib/components/Nav.svelte";
 </script>
 
+<svelte:head />
+
 <div>
   <Nav />
   <slot />
 </div>
+
+<style>
+  div {
+    width: 100%;
+    padding-left: 4rem;
+    padding-right: 4rem;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  @media (min-width: 576px) {
+    div {
+      width: 540px;
+    }
+  }
+  @media (min-width: 768px) {
+    div {
+      width: 720px;
+    }
+  }
+</style>
