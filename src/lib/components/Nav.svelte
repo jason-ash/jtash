667784648a7895b9b1585c0cba2a4d46c0196914
Jason Ash <ash.jasont@gmail.com>
2021-12-11 17:16:56 -0800

    add styling to navbar

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,11 +1,36 @@
 <div>
   <nav>
-    <div>
-      <a href="/">Jason Ash</a>
-    </div>
-    <div>
-      <a href="/about">About</a>
-      <a href="/resume">Resume</a>
-    </div>
+    <ul>
+      <li><a href="/">Jason Ash</a></li>
+    </ul>
+    <ul>
+      <li><a href="/about">About</a></li>
+      <li><a href="/resume">Resume</a></li>
+    </ul>
   </nav>
 </div>
+
+<style>
+  nav {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+  }
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+  li {
+    display: inline;
+    text-align: center;
+  }
+  li:not(:last-child) {
+    padding-right: 1.5rem;
+  }
+  li a {
+    text-decoration: none;
+  }
+</style>
