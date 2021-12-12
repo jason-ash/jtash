21d24db70e0bd65f1d1a145f18f293a03d75996a
Jason Ash <ash.jasont@gmail.com>
2021-12-11 17:38:53 -0800

    add basic styling for PostCards

--- src/lib/components/PostCard.svelte
+++ src/lib/components/PostCard.svelte
@@ -6,11 +6,26 @@
 </script>
 
 <div>
-  <div>
+  <div class="title">
     <a href={`${base}/${post.slug}`}>
       <h2>{post.title}</h2>
     </a>
     <p>{post.date}</p>
   </div>
-  <p>{post.excerpt}</p>
+  <p class="excerpt">{post.excerpt}</p>
 </div>
+
+<style>
+  .title {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 2rem;
+  }
+  .title a {
+    text-decoration: none;
+  }
+  .excerpt {
+    margin-block-start: 0rem;
+  }
+</style>
