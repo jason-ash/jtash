3efa63b6c63d9a7f781951d02a738bad806e672c
Jason Ash <ash.jasont@gmail.com>
2021-12-19 13:28:26 -0800

    add a new component for post styling

---
+++ src/lib/components/PostCardTwoColumn.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import type { postType } from "$lib/posts";
+  import { formatDateString } from "$lib/posts";
+  import { base } from "$app/paths";
+
+  export let post: postType;
+</script>
+
+<a href={`${base}/${post.slug}`}>
+  <div class="title">
+    <div class="header">
+      <h2>{post.title}</h2>
+      <p>{formatDateString(post.date)}</p>
+    </div>
+    <p class="excerpt">{@html post.excerpt}</p>
+  </div>
+</a>
+
+<style>
+  .header {
+    flex: 0 0 20%;
+    margin-right: 2rem;
+  }
+  .header p {
+    margin-block-start: 0.5rem;
+  }
+  .title {
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    margin-top: 2rem;
+    margin-bottom: 0.5rem;
+  }
+  a {
+    text-decoration: none;
+    border-bottom: none;
+  }
+  a:hover h2 {
+    text-decoration: underline;
+  }
+  .title h2 {
+    margin-block-end: 0rem;
+    margin-block-start: 0rem;
+  }
+  .title p {
+    font-weight: 400;
+  }
+  .excerpt {
+    font-weight: 400;
+    margin-block-start: 0rem;
+  }
+</style>
