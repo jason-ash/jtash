b5858541932835223b33192c328f90e4f14724d6
Jason Ash <ash.jasont@gmail.com>
2021-12-23 20:30:01 -0800

    update hero intro text

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,8 +1,16 @@
 <div>
   <h1>Hello! I'm Jason.</h1>
   <p>
-    I created this site to write about things that I find interesting: probability &
-    Bayesian inference, data visualization, puzzles & games, finance, and books.
+    I'm a data scientist, actuary, and self-taught software engineer based in San
+    Francisco. Currently I lead a 40-person data & analytics team at Symetra, an
+    insurance company.
+  </p>
+  <p>
+    I created this site to share the things I'm interested in. You'll find posts
+    explaining how I built this site, how I solved programming puzzles using Python, and
+    how I use math to understand the world - like using Bayesian inference to analyze my
+    daily step count before and after I got a German Shepherd puppy. You can find me on
+    Twitter or Github - let's start a conversation!
   </p>
 </div>
 <hr />
@@ -13,5 +21,6 @@
   }
   p {
     margin-block-start: 0rem;
+    hyphens: auto;
   }
 </style>
