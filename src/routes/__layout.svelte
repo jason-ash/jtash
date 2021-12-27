8a1d6fb999b42428bbda7acbf846636c2f863d03
Jason Ash <ash.jasont@gmail.com>
2021-12-27 10:35:20 -0800

    move container css to app.css

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,28 +4,8 @@
   import Nav from "../lib/components/Nav.svelte";
 </script>
 
-<div>
+<div class="container">
   <Nav />
   <slot />
   <Footer />
 </div>
-
-<style>
-  div {
-    width: 100%;
-    padding-left: 4rem;
-    padding-right: 4rem;
-    margin-left: auto;
-    margin-right: auto;
-  }
-  @media (min-width: 576px) {
-    div {
-      width: 540px;
-    }
-  }
-  @media (min-width: 768px) {
-    div {
-      width: 720px;
-    }
-  }
-</style>
