00bc1cf0e711f32f67b6ac7bd34a584b7f56fc47
Jason Ash <ash.jasont@gmail.com>
2021-12-28 12:36:12 -0800

    add typing to json get functions

--- src/routes/[slug].json.ts
+++ src/routes/[slug].json.ts
@@ -1,6 +1,7 @@
+import type { RequestHandler } from "@sveltejs/kit";
 import { getPostBySlug } from "$lib/posts";
 
-export const get = ({ params }): { body: string } => {
+export const get: RequestHandler = ({ params }): { body: string } => {
   const { slug } = params;
   const post = getPostBySlug(slug);
   const body = JSON.stringify(post);
--- src/routes/index.json.ts
+++ src/routes/index.json.ts
@@ -1,6 +1,7 @@
+import type { RequestHandler } from "@sveltejs/kit";
 import { getAllPosts } from "$lib/posts";
 
-export const get = (): { body: string } => {
+export const get: RequestHandler = (): { body: string } => {
   const posts = getAllPosts();
   const body = JSON.stringify(posts);
   return { body };
