1244d64e09dbace3b59002817dfad7bada1f3f67
Jason Ash <ash.jasont@gmail.com>
2021-12-27 21:51:47 -0800

    move highlightjs code to app.css

--- src/lib/components/PostLayout.svelte
+++ src/lib/components/PostLayout.svelte
@@ -8,10 +8,6 @@
 <svelte:head>
   <link
     rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/github.min.css"
-  />
-  <link
-    rel="stylesheet"
     href="https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.min.css"
     integrity="sha384-R4558gYOUz8mP9YWpZJjofhk+zx0AS11p36HnD2ZKj/6JR5z27gSSULCNHIRReVs"
     crossorigin="anonymous"
