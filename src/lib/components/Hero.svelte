f051139644374554363233f235120a967f42511a
Jason Ash <ash.jasont@gmail.com>
2021-12-30 17:15:56 -0800

    update hero intro text

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,15 +1,15 @@
 <div>
   <p>
     <strong>Hello! I'm Jason.</strong> I'm a data scientist, actuary, and self-taught software
-    engineer based in San Francisco. Currently I lead the enterprise data & analytics team
-    at Symetra, an insurance company.
+    engineer based in San Francisco. Currently I lead the data & analytics team at Symetra,
+    an insurance company.
   </p>
   <p>
     I created this site to share the things I'm interested in. You'll find posts
     explaining how I built this site, how I write code and solve puzzles using Python,
     and how I use math to understand the world - like using Bayesian inference to analyze
-    my daily step count before and after I got a German Shepherd puppy. You can find me
-    on Twitter or Github - let's start a conversation!
+    my daily step count before and after I got a German Shepherd puppy. You can also find
+    me on Twitter or Github - let's start a conversation!
   </p>
 </div>
 <hr />
