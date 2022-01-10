daa5157a56133050de12b748cf009904fd1ab2d9
Jason Ash <ash.jasont@gmail.com>
2022-01-09 19:38:30 -0800

    add under construction banner to intro

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -11,10 +11,30 @@
     my daily step count before and after I got a German Shepherd puppy. You can also find
     me on Twitter or Github - let's start a conversation!
   </p>
+  <div class="construction-banner">
+    <p>
+      🚧 Quick note - I've been working on a redesign of this site, but it's a work in
+      progress. Feel free to poke around, but beware there might be a few sharp edges! 🚧
+    </p>
+  </div>
 </div>
 <hr />
 
 <style>
+  .construction-banner {
+    padding-left: 3rem;
+    padding-right: 3rem;
+    background: repeating-linear-gradient(
+      45deg,
+      #fffbeb,
+      #fffbeb 10px,
+      #fef3c7 10px,
+      #fef3c7 20px
+    );
+  }
+  .construction-banner p {
+    text-align: center;
+  }
   p {
     line-height: 1.5;
     margin-block-start: 0rem;
