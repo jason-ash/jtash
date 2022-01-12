6452a69e4981e89bdcc1ba18a5bf4847af4ac40c
Jason Ash <ash.jasont@gmail.com>
2022-01-11 21:37:07 -0800

    replace post layout component with coding directly in [slug].svelte (#42)

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -15,11 +15,54 @@
 </script>
 
 <script lang="ts">
-  import PostLayout from "$lib/components/PostLayout.svelte";
   import TwitterSummaryCard from "$lib/components/SEO/TwitterSummaryCard.svelte";
   export let post: postType;
-  console.log(post);
 </script>
 
+<svelte:head>
+  <title>{post.title}</title>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.min.css"
+    integrity="sha384-R4558gYOUz8mP9YWpZJjofhk+zx0AS11p36HnD2ZKj/6JR5z27gSSULCNHIRReVs"
+    crossorigin="anonymous"
+  />
+  <script
+    defer
+    src="https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.min.js"
+    integrity="sha384-z1fJDqw8ZApjGO3/unPWUPsIymfsJmyrDVWC8Tv/a1HeOtGmkwNd/7xUS0Xcnvsx"
+    crossorigin="anonymous">
+  </script>
+</svelte:head>
+
 <TwitterSummaryCard {post} />
-<PostLayout {post} />
+
+<div>
+  <div class="title">
+    <h1>{post.title}</h1>
+    <p>{post.date}</p>
+  </div>
+  <hr />
+  <div class="post">
+    {@html post.content}
+  </div>
+</div>
+
+<style>
+  .title {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 1rem;
+  }
+  .title h1 {
+    margin-block-end: 0.4rem;
+    margin-block-start: 0.4rem;
+  }
+  .title p {
+    color: var(--text-color-secondary);
+  }
+  .post :global(h1) {
+    font-size: 1.35em;
+  }
+</style>
